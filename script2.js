0a079fda11ef91554983c9bbed0681f4eacd98b6
Michelle Jensen <mchlljnsn@yahoo.com>
2019-10-15 15:53:22 -0400

    Tuesday First Commit

--- script2.js
+++ script2.js
@@ -1,77 +1,54 @@
 // Assign ID for each square in grid
 
 let playerTurn = "X";
-
-let moves = [];
-
+let counter = 0
+let moves = ['','','','','','','','',''];
 
 const squares = document.querySelectorAll(".square");
 for (i=0; i < squares.length; i++) {
     squares[i].id = i;
     squares[i].addEventListener("click", function() {
+        this.innerText = playerTurn;
         moves[this.id] = playerTurn;
+        counter++
+        if (counter > 4) {
+            checkForWinner()
+        }        
         if (playerTurn === "X") {
             playerTurn = "O";
         } else {
             playerTurn = "X";
         }
-        console.log(moves);
+        // console.log(moves);
     });
 }
 
-// Assign click listener to each square, button and X and O
-
-document.getElementById(".button").addEventListener("click", function() {
-
-});
-
-const clicked() {
-    alert(document.getElementById("squares").innerText)
-}
-
-//Create empty array for player selections
-
-const playerSelection = [];
-
 // Create callback function to get square ID# and send it to empty array
-
-function selected(squares, callback) {
-
-    callback();
-}
 
 // Create innerText function to record each X or O click
 
-
-
 // For loop to determine winner
 
-for(let i = 0; i <= 9; i++)
-    if (0 === 1  && 1 === 2 || 2 === 1 && 1 === 0) {
-        alert("X wins!");
-    } else if (3 === 4 && 4 === 5 || 5 === 4 && 4 === 3) {
-        alert("X wins!");
-    } else if (6 === 7 && 7 === 8 || 8 === 7 && 7 === 6) {
-        alert("X wins!");
-    } else if (0 === 3 && 3 === 6 || 6 === 3 && 3 === 0) {
-        alert("X wins!");
-    } else if (1 === 4 && 4 === 7 || 7 === 4 && 4 === 1) {
-        alert("X wins!");
-    } else if (2 === 5 && 5 === 8 || 8 === 5 && 5 === 2) {
-        alert("X wins!");
-    } else if (0 === 4 && 4 === 8 || 8 === 4 && 4 === 0) {
-        alert("X wins!");
-    } else if (2 === 4 && 4 === 6 || 6 === 4 && 4 === 2) {
-        alert("X wins!");
-    } else alert("Draw!");
-
-document.querySelectorAll(square)
-if (square === "X" || "O") {
-    square === " ";
+function checkForWinner() {
+    if (moves[0] === moves[1] && moves[1] === moves[2] && moves[0] !== '') {
+        console.log(`${playerTurn} wins!`);
+    } else if (moves[3] === moves[4] && moves[4] === moves[5] && moves[3] !== '') {
+        console.log(`${playerTurn} wins!`);
+    } else if (moves[6] === moves[7] && moves[7] === moves[8] && moves[6] !== '') {
+        console.log(`${playerTurn} wins!`);
+    } else if (moves[0] === moves [3] && moves[3] === moves[6] && moves[0] !== '') {
+        console.log(`${playerTurn} wins!`);
+    } else if (moves [1] === moves [4] && moves[4] === moves[7] && moves[1] !== '') {
+        console.log(`${playerTurn} wins!`);
+    } else if (moves[2] === moves[5] && moves[5] === moves[8] && moves[2] !== '') {
+        console.log(`${playerTurn} wins!`);
+    } else if (moves[0] === moves[4] && moves[4] === moves[8] && moves[0] !== '') {
+        console.log(`${playerTurn} wins!`);
+    } else if (moves[2] === moves[4] && moves[4] === moves[6] && moves[2] !== '') {
+        console.log(`${playerTurn} wins!`);
+    } else if (counter === 9) {
+        console.log("Draw! Start new game.");
+    } else {
+        return
+    }
 }
-
-
-
-
-
-
