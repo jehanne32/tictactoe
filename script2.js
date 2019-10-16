548625ff37b5b08ad8194d6fcb88de5b5e7ce4a8
Michelle Jensen <mchlljnsn@yahoo.com>
2019-10-16 15:35:38 -0400

    Wednesday Sixth Commit

--- script2.js
+++ script2.js
@@ -2,17 +2,22 @@
 
 let playerTurn = "X";
 let counter = 0
+// Create empty array to hold all player moves
 let moves = ['','','','','','','','',''];
 
 // function to loop through filled in squares and check for winner
 
 const squares = document.querySelectorAll(".square");
+// Loop through all the squares 
 for (i=0; i < squares.length; i++) {
     squares[i].id = i;
+// Add event listener, click, to each square
     squares[i].addEventListener("click", function() {
         this.innerText = playerTurn;
-        moves[this.id] = playerTurn;    
+        moves[this.id] = playerTurn;
+ // Add counter to count squares clicked           
         counter++
+// Do not start counter until 5 squares have been clicked
         if (counter > 4) {
             checkForWinner()
         }        
@@ -24,7 +29,7 @@ for (i=0; i < squares.length; i++) {
     });
 }
 
-// For loop to determine winner
+// If statements to determine winner or draw
 // Alerts to notify winner or draw
 
 function checkForWinner() {
@@ -50,6 +55,8 @@ function checkForWinner() {
         return
     }
 }
+
+// Create new variable and function to reset grid to clear after game completion
 
 const emptyGrid = document.querySelector(".button");
    emptyGrid.addEventListener("click", function() {
