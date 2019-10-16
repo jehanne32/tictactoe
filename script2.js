5cad509c0510fd3e7d56c456601e6a3ae2497ac6
Michelle Jensen <mchlljnsn@yahoo.com>
2019-10-16 16:44:25 -0400

    Wednesday Fifth Commit

--- script2.js
+++ script2.js
@@ -1,4 +1,4 @@
-// Assign ID for each square in grid
+// Create variable to hold player turn and counter to count through squares in grid
 
 let playerTurn = "X";
 let counter = 0
@@ -16,39 +16,44 @@ for (i=0; i < squares.length; i++) {
         this.innerText = playerTurn;
         moves[this.id] = playerTurn;
  // Add counter to count squares clicked           
-        counter++
+       
 // Do not start counter until 5 squares have been clicked
-        if (counter > 4) {
-            checkForWinner()
+        if (counter > 3) {
+            setTimeout(checkForWinner, 500)  
         }        
         if (playerTurn === "X") {
             playerTurn = "O";
         } else {
             playerTurn = "X";
+
         }
+        counter++
+    }, {
+        once: true
+
     });
 }
 
-// If statements to determine winner or draw
+// If statements to determine winner or draw with && and === and !== ''
 // Alerts to notify winner or draw
 
 function checkForWinner() {
     if (moves[0] === moves[1] && moves[1] === moves[2] && moves[0] !== '') {
-        alert(`${playerTurn} wins!`);
+        alert(`${moves[0]} wins!`); 
     } else if (moves[3] === moves[4] && moves[4] === moves[5] && moves[3] !== '') {
-        alert(`${playerTurn} wins!`);
+        alert(`${moves[3]} wins!`);
     } else if (moves[6] === moves[7] && moves[7] === moves[8] && moves[6] !== '') {
-        alert(`${playerTurn} wins!`);
+        alert(`${moves[6]} wins!`);
     } else if (moves[0] === moves [3] && moves[3] === moves[6] && moves[0] !== '') {
-        alert(`${playerTurn} wins!`);
+        alert(`${moves[0]} wins!`);
     } else if (moves [1] === moves [4] && moves[4] === moves[7] && moves[1] !== '') {
-        alert(`${playerTurn} wins!`);
+        alert(`${moves[1]} wins!`);
     } else if (moves[2] === moves[5] && moves[5] === moves[8] && moves[2] !== '') {
-        alert(`${playerTurn} wins!`);
+        alert(`${moves[2]} wins!`);
     } else if (moves[0] === moves[4] && moves[4] === moves[8] && moves[0] !== '') {
-        alert(`${playerTurn} wins!`);
+        alert(`${moves[0]} wins!`);
     } else if (moves[2] === moves[4] && moves[4] === moves[6] && moves[2] !== '') {
-        alert(`${playerTurn} wins!`);
+        alert(`${moves[2]} wins!`);
     } else if (counter === 9) {
         alert("Draw! Start new game.");
     } else {
@@ -60,11 +65,11 @@ function checkForWinner() {
 
 const emptyGrid = document.querySelector(".button");
    emptyGrid.addEventListener("click", function() {
- //       console.log("Button clicked");
-   let square = document.querySelectorAll(".squares", function() {
-          for (i = 0; i <= squares.length; i++) {                   
+        console.log("Button clicked");
+//   let squares = document.querySelectorAll(".square")
+
+          for (i = 0; i < squares.length; i++) {                   
                    squares[i].innerText = " ";
    }
 
 })        
-})
